a5560c5f8e3c9b99be62cd39f2db235197840420
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-18 22:21:03 +0800

    working on item card

--- src/YourProduct/components/listproducts.jsx
+++ src/YourProduct/components/listproducts.jsx
@@ -1,20 +1,23 @@
 import ProductCard from "./productCard"
 import '../style/yourProductlist.css'
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 function ListProducts() {
+    var [loadedProductImage, setloadedProductImage] = useState(['./images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png'])
+    var [loadedProductName, setloadedProductName] = useState(['bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf'])
+    var [loadedProductPrice, setloadedProductPrice] = useState([200,200,200,200,200,200,200,200])
+    
     return (
         <>
             <div className="pGridView">
                 <div className="productGrid">
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    {
+                        loadedProductImage.map((item, index) => (
+                            <ProductCard imgsrc={item} prodName={loadedProductName[index]} prodPrice={loadedProductPrice[index]} key={index} />
+                        ))
+                    }
                 </div>
             </div>
         </>
--- src/YourProduct/components/productCard.jsx
+++ src/YourProduct/components/productCard.jsx
@@ -1,11 +1,26 @@
 import '../style/yourProductlist.css'
 import { useNavigate } from "react-router-dom";
 
-function ProductCard() {
+function ProductCard(props) {
     return (
         <>
             <div className="cardProd">
-                
+                <div style={{"height":"225px","overflow":"hidden"}}>
+                    <img src={props.imgsrc} className='prodCardImage'></img>
+                </div>
+                <div>
+                    <div>
+                        {props.prodName}
+                    </div>
+                    <div style={{"display":"flex","flexDirection":"row"}}>
+                        <div>
+                            P
+                        </div>
+                        <div>
+                            {props.prodPrice}
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )

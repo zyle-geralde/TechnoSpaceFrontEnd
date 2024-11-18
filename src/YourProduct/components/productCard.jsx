43cf329abd6f5a40aa679495049a0c31391a0c49
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-18 22:47:52 +0800

    finisehd learning resources page

--- src/YourProduct/components/productCard.jsx
+++ src/YourProduct/components/productCard.jsx
@@ -5,11 +5,11 @@ function ProductCard(props) {
     return (
         <>
             <div className="cardProd">
-                <div style={{"height":"225px","overflow":"hidden"}}>
+                <div style={{"height":"215px","overflow":"hidden"}}>
                     <img src={props.imgsrc} className='prodCardImage'></img>
                 </div>
-                <div>
-                    <div>
+                <div style={{"marginTop":"10px","marginRight":"10px","marginLeft":"10px"}}>
+                    <div className='prodCardName'>
                         {props.prodName}
                     </div>
                     <div style={{"display":"flex","flexDirection":"row"}}>

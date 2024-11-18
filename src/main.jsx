20e18ea863a5dd92d64d16c14ec3031e5c01a723
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-18 16:57:42 +0800

    configured files

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import BookList from './bookviews/BookList.jsx'
 import { Routes, Route, Router, BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(

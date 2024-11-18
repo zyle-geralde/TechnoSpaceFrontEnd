8ddef8534f36b87107b5fb51ccd853328ef5e6fb
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-18 23:19:11 +0800

    added route for shool supply page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route, Router, BrowserRouter } from 'react-router-dom';
 import ListingView from './YourProduct/pages/ChooseListingView';
 import LearningResourcesPage from './YourProduct/pages/LearningResourcesPage';
 import NotfundFunc from './NotFound/notfound';
+import SchoolSupplyPage from './YourProduct/pages/SchoolSuppliespage';
 
 function App() {
     return (
         <Routes>
             <Route path="/Yourproducts" element={<ListingView></ListingView>} />
             <Route path="/Learningresourcesview" element={<LearningResourcesPage />} />
+            <Route path="/Schoolsuppliesview" element={<SchoolSupplyPage />} />
             <Route path="*" element={<NotfundFunc />} />
 
         </Routes>

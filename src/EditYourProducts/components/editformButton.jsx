20426cb3436fc0e6e45216238d45b85698f78883
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-19 21:39:29 +0800

    added modal

--- src/EditYourProducts/components/editformButton.jsx
+++ src/EditYourProducts/components/editformButton.jsx
@@ -1,10 +1,77 @@
 import '../styles/editlisting.css'
+import React, { useRef } from "react";
+
 
 function EditFormButton(props) {
+    const modalRef = useRef(null);
+
+    const handleShowModal = () => {
+        if (modalRef.current) {
+          // Use Bootstrap's modal method to show the modal
+          window.$(modalRef.current).modal("show");
+        }
+      };
+
+    
+    function chooseButton() {
+        if (props.ButtonName === "Delete") {
+            return(<div className={props.classIndic} data-toggle="modal" data-target="#exampleModal" onClick={handleShowModal}>
+                {props.ButtonName}
+            </div>)
+        }
+        else {
+            return(<div className={props.classIndic}>
+                {props.ButtonName}
+            </div>)
+        }
+    }
     return (
-        <div className={props.classIndic}>
-            {props.ButtonName}
+        <>
+            {chooseButton()}
+
+            <div
+        ref={modalRef}
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Modal title
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">This is a dynamically triggered modal.</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
         </div>
+      </div>
+
+            
+        </>
     )
 }
 
--- src/EditYourProducts/components/formfillcomp.jsx
+++ src/EditYourProducts/components/formfillcomp.jsx
@@ -3,17 +3,18 @@ import FormLabelComp from './formlabelcomp'
 import EditFormButton from './editformButton'
 function FormFillComp() {
     return (
-        <div style={{"marginLeft":"50px"}}>
-            <FormLabelComp formName="Title" typeform = "formT"/>
+        <div style={{ "marginLeft": "50px" }}>
+            <FormLabelComp formName="Title" typeform="formT" />
             <FormLabelComp formName="Project Description" typeform="textT" />
             <FormLabelComp formName="Project Specification" typeform="textT" />
             <FormLabelComp formName="Available Schedule" typeform="textT" />
             <FormLabelComp formName="Location" typeform="formT" />
             <FormLabelComp formName="Price" typeform="formnumT" />
-            <div style={{ "display": "flex", "flexDirection": "row","marginTop":"20px"}}>
+            <div style={{ "display": "flex", "flexDirection": "row", "marginTop": "20px" }}>
                 <EditFormButton classIndic={"cancelBut"} ButtonName={"Delete"} />
-                <EditFormButton classIndic={"doneBut"} ButtonName={"Done"}/>
+                <EditFormButton classIndic={"doneBut"} ButtonName={"Done"} />
             </div>
+
         </div>
     )
 }

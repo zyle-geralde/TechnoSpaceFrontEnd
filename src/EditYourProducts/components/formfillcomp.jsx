4ddedc93eeeec5a5d922920103b69a5bc0cad013
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-19 18:29:08 +0800

    finished forms

--- src/EditYourProducts/components/formfillcomp.jsx
+++ src/EditYourProducts/components/formfillcomp.jsx
@@ -3,7 +3,12 @@ import FormLabelComp from './formlabelcomp'
 function FormFillComp() {
     return (
         <div style={{"marginLeft":"50px"}}>
-            <FormLabelComp formName = "Title"/>
+            <FormLabelComp formName="Title" typeform = "formT"/>
+            <FormLabelComp formName="Project Description" typeform="textT" />
+            <FormLabelComp formName="Project Specification" typeform="textT" />
+            <FormLabelComp formName="Available Schedule" typeform="textT" />
+            <FormLabelComp formName="Location" typeform="formT" />
+            <FormLabelComp formName="Price" typeform = "formnumT"/>
         </div>
     )
 }
--- src/EditYourProducts/components/formlabelcomp.jsx
+++ src/EditYourProducts/components/formlabelcomp.jsx
@@ -1,13 +1,34 @@
 import '../styles/editlisting.css'
 
 function FormLabelComp(props) {
+    var formshow;
+
+    function chooseFormType() {
+        if (props.typeform == "formT") {
+            return (
+                <input type="text" className="formInputStyle"></input>
+            )
+        }
+        else if (props.typeform == "formnumT") {
+            return (
+                <input type="number" className="formInputStyle" min={0}></input>
+            )
+        }
+        else {
+            return (
+                <textarea className='formTextAreastyle'></textarea>
+            )
+        }
+    }
+
     return (
-        <div>
+        <div className = "formContMarg">
             <div className='formNamestyle'>
                 {props.formName}
             </div>
             <div>
-                <input type="text" className="formInputStyle"></input>
+                {chooseFormType()}
+                
             </div>
         </div>
 

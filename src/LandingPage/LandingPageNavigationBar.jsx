2873f6f4d1fa3f776557caf2b38127deee9d0271
ardel <ardeltiocojefflauron@gmail.com>
2024-11-25 19:37:11 +0800

    (Lauron) Added 2 sections

--- src/LandingPage/LandingPageNavigationBar.jsx
+++ src/LandingPage/LandingPageNavigationBar.jsx
@@ -1,17 +1,18 @@
 function LandingPageNavigationBar() {
     return (
-        <div id="landing-nav" className="flex-horizontal py-2 px-20 border-2 border-b-primary-color">
-                <ul id="left-part" className="flex-horizontal w-1/2 gap-4">
-                    <li><a className="nav-text-hover" href="#Home">Home</a></li>
-                    <li><a className="nav-text-hover" href="#About-us">About Us</a></li>
-                    <li><a className="nav-text-hover" href="#Contact-us">Contact Us</a></li>
-                </ul>
-
-                <div id="right-part" className="w-1/2 flex-horizontal justify-end gap-4">
-                    <button className="border-2 px-5">Join</button>
-                    <button className="px-5 bg-primary-color text-white rounded-sm">Shop</button>
-                </div>
+    <div className="sticky top-0 z-50 backdrop-blur-sm border-3 border-b-primary-color p-2">
+        <div className="text-xs flex flex-row self-center sm:px-10 md:px-20 lg:px-30 xl:px-40 ">
+            <ul className="flex gap-4 self-center w-full justify-center sm:w-fit">
+                <li><a className="nav-text-hover" href="#introduction">Home</a></li>
+                <li><a className="nav-text-hover" href="#about-us">About Us</a></li>
+                <li><a className="nav-text-hover" href="#contact-us">Contact Us</a></li>
+            </ul>
+            <div className="ml-auto gap-4 hidden sm:flex">
+                <button className="border-2 px-3 text-xs hover:bg-slate-200">Join</button>
+                <button className="px-3 bg-primary-color text-white rounded-sm text-xs hover:bg-red-600">Shop</button>
+            </div>
         </div>
+    </div>
     )
 }
 

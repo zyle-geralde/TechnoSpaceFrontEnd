8f25b1b5abe9796115754ab400c49f117b756e4a
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-30 00:54:36 +0800

    firsthalf of view product finished

--- src/ViewProduct/pages/viewProductPage.jsx
+++ src/ViewProduct/pages/viewProductPage.jsx
@@ -1,7 +1,35 @@
-
+import "../styles/viewproductstyle.css"
+import HeaderComp from "../../YourProduct/components/header";
 
 function ViewProductPage() {
     return <>
+        <div className="divmain">
+            <HeaderComp/>
+            <div className="contianmainProd">
+                <div className="firstHalfprod">
+                    <div className="mainImageCont">
+                        <img src = "./images/defaultImage.png" className="mainImage"></img>
+                    </div>
+                    <div className="subImageCont">
+                        <div className="subImagesubCont">
+                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                        </div>
+                        <div className="subImagesubCont">
+                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                        </div>
+                        <div className="subImagesubCont">
+                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                        </div>
+                        <div className="subImagesubCont">
+                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                        </div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
     </>
 }
 

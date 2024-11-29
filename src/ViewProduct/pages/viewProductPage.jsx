3a7f8f941349222929e081b19471dfa4d6edcb16
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-30 03:45:25 +0800

    finished Product View

--- src/ViewProduct/pages/viewProductPage.jsx
+++ src/ViewProduct/pages/viewProductPage.jsx
@@ -2,13 +2,27 @@ import "../styles/viewproductstyle.css"
 import HeaderComp from "../../YourProduct/components/header";
 import BackComp from "../../EditYourProducts/components/backcomp";
 import { useNavigate } from "react-router-dom";
+import React, { useRef } from "react";
 
 function ViewProductPage() {
+    const modelSell = useRef(null);
+    
+    const handleShowModalsell = () => {
+        if (modelSell.current) {
+          window.$(modelSell.current).modal("show");
+        }
+      };
+      const handleCloseModalsell = () => {
+        if (modelSell.current) {
+          window.$(modelSell.current).modal("hide");
+        }
+      };
     const navigate = useNavigate();
 
     function goback() {
         navigate("/BrowseProduct")
     }
+
     return <>
         <HeaderComp />
         <div style={{"position":"absolute","top":"60px","left":"30px"}}>
@@ -82,10 +96,45 @@ function ViewProductPage() {
                             asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnk
                         </div>
                     </div>
-                    <div className="PurchProd">Purchase Prodcut</div>
+                    <div className="PurchProd" onClick={handleShowModalsell}>Purchase Product</div>
                 </div>
             </div>
         </div>
+
+        <div
+        ref={modelSell}
+        className="modal fade"
+        id="sellModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="sellModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header" style={{"display":"flex","flexDirection":"row","justifyContent":"center"}}>
+              <h5 className="modal-title" id="sellModalLabel" style={{"fontWeight":"bold","fontSize":"18px"}}>
+                Confirm
+              </h5>
+            </div>
+            <div className="modal-body" style={{"display":"flex","flexDirection":"row","justifyContent":"center"}}>Are you sure you want to buy?</div>
+            <div className="modal-footer" style={{"display":"flex","flexDirection":"row","justifyContent":"center"}}>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+                aria-label="Close" onClick={handleCloseModalsell}
+                style={{"backgroundColor":"white","borderStyle":"solid","borderWidth":"1px","borderColor":"black","color":"black"}}        
+              >
+                Cancel
+              </button>
+              <button type="button" className="btn" style={{"backgroundColor":"#800000c4","color":"white"}}>
+                Confirm
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
 }
 

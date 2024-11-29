0c120deda56546d848eb965fa174d44bbcc915f4
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-30 01:54:28 +0800

    second half indormation done

--- src/ViewProduct/pages/viewProductPage.jsx
+++ src/ViewProduct/pages/viewProductPage.jsx
@@ -3,30 +3,74 @@ import HeaderComp from "../../YourProduct/components/header";
 
 function ViewProductPage() {
     return <>
+        <HeaderComp />
         <div className="divmain">
-            <HeaderComp/>
             <div className="contianmainProd">
                 <div className="firstHalfprod">
                     <div className="mainImageCont">
-                        <img src = "./images/defaultImage.png" className="mainImage"></img>
+                        <img src="./images/defaultImage.png" className="mainImage"></img>
                     </div>
                     <div className="subImageCont">
                         <div className="subImagesubCont">
-                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                            <img src="./images/defaultImage.png" className="subImage"></img>
                         </div>
                         <div className="subImagesubCont">
-                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                            <img src="./images/defaultImage.png" className="subImage"></img>
                         </div>
                         <div className="subImagesubCont">
-                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                            <img src="./images/defaultImage.png" className="subImage"></img>
                         </div>
                         <div className="subImagesubCont">
-                            <img src = "./images/defaultImage.png" className="subImage"></img>
+                            <img src="./images/defaultImage.png" className="subImage"></img>
                         </div>
                     </div>
                 </div>
-                <div>
-
+                <div className="secondHalfprod">
+                    <div className="prodTitle">
+                        Long bond paper good product
+                    </div>
+                    <div className="prodPrice">
+                        PHP 150
+                    </div>
+                    <div className="buttonGroupClass">
+                        <div className="messageBut">
+                            Message Seller
+                        </div>
+                        <div className="reportBut">
+                            Report
+                        </div>
+                    </div>
+                    <div className="containInfo">
+                        <div style={{"fontWeight":"bold","marginTop":"10px"}}>Product Description</div>
+                        <div className="ProdDesc">
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                        </div>
+                        <div style={{"fontWeight":"bold","marginTop":"10px"}}>Product Specification</div>
+                        <div className="ProdSpecification">
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                        </div>
+                        <div style={{"fontWeight":"bold","marginTop":"10px"}}>Available Schedule</div>
+                        <div className="AveSched">
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajfasjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnksajf
+                        </div>
+                        <div style={{"fontWeight":"bold","marginTop":"10px"}}>Location</div>
+                        <div className="LocProd">
+                            asjkdfnjksanfkjsndfkjsandkfjnasdkjfnasdjkfndaskjnk
+                        </div>
+                    </div>
+                    <div className="PurchProd">Purchase Prodcut</div>
                 </div>
             </div>
         </div>

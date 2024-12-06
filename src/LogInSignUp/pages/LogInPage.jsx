e715c5d0e45a46911f9ab19951c694b0a5ee050b
zyle-geralde <zylgerdlp19@gmail.com>
2024-12-07 03:24:58 +0800

    finished log in

--- src/LogInSignUp/pages/LogInPage.jsx
+++ src/LogInSignUp/pages/LogInPage.jsx
@@ -5,11 +5,13 @@ import ButtonClickComp from "../components/buttonClickComp";
 import ChangeLocComp from "../components/ChangelocComp";
 import axios from 'axios'
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LogInPage() {
     const [email_id, setEmail_ID] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -22,9 +24,22 @@ function LogInPage() {
             //replace with azure api
             const response = await axios.post('https://localhost:7016/api/Login',payload);
             setMessage('login success');
-            alert(`Response: ${JSON.stringify(response.data)}`);
+            navLink("/BrowseProduct")
+            
         } catch (error) {
-            alert(`Error: ${error.message}`);
+            if (error.response.status == "400") {
+                alert("Invalid Credentials")
+            }
+            else if(error.response.status == "404") {
+                alert("Invalid Credentials")
+            }
+            else if (error.response.status == "401") {
+                alert("Invalid Credentials")
+            }
+            else {
+                alert(error.message)
+            }
+            navLink("/Login")
             console.log("Email/ID:", email_id, "Password:", password);
         }
         
@@ -39,12 +54,16 @@ function LogInPage() {
         console.log(password)
     }
 
+    function navLink(linktoNav) {
+        navigate(linktoNav)
+    }
+
     return <>
         <div className="LogInMainCont">
             <div className="logInCont">
                 <div className="logInformCont">
                     <TitleLogComp title="LogIn" />
-                    <InputLogComp type="email" classinp="emailInp" labelInp="Email address" onChangeF={setUserEmail} />
+                    <InputLogComp type="email" classinp="emailInp" labelInp="Email address" onChangeF={setUserEmail}/>
                     <InputLogComp type="password" classinp="passwordInp" labelInp="Password" onChangeF={setUserPassword}/>
                     <ButtonClickComp classBut="clickButton" titleBut="LogIn" onFunc={handleLogin} />
                     <ChangeLocComp message="Don't have an account?" changelink="/SignUp" indic="Sing up" />

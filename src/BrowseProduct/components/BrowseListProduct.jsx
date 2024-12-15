feeb48c42f01d993d13d8be6e0b57098ce806ba8
zyle-geralde <zylgerdlp19@gmail.com>
2024-12-16 01:36:53 +0800

    started Browse product

--- src/BrowseProduct/components/BrowseListProduct.jsx
+++ src/BrowseProduct/components/BrowseListProduct.jsx
@@ -2,20 +2,49 @@ import ProductCard from '../../YourProduct/components/productCard';
 import "../styles/browseProd.css"
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useEffect } from 'react';
+import axios from 'axios';
 
 
 function BrowseListProducts(props) {
     var [loadedProductImage, setloadedProductImage] = useState(['./images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png', './images/sampleProduct.png'])
     var [loadedProductName, setloadedProductName] = useState(['bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf', 'bondbondpaaperpaeradjfnsjadfnkjsandf'])
-    var [loadedProductPrice, setloadedProductPrice] = useState([200,200,200,200,200,200,200,200])
+    var [loadedProductPrice, setloadedProductPrice] = useState([200, 200, 200, 200, 200, 200, 200, 200])
+
+    const creatorEmail = localStorage.getItem("email");
+    
+    var [allresponse, setAllResponse] = useState([])
+    var [holdAllresp, setholdAllresp] = useState([])
+
+    async function fetchAllProducts() {
+        try {
+            const response = await axios.get("https://localhost:7016/api/Products/all");
+            console.log(response.data)
+
+            setAllResponse(response.data)
+            setholdAllresp(response.data)
+        } catch (err) {
+            console.error("Error fetching products:", err);
+        }
+    }
+
+    useEffect(() => {
+
+        fetchAllProducts();
+    }, []);
     
     return (
         <>
             <div className="pGridView">
                 <div className="productGrid p-2">
                     {
-                        loadedProductImage.map((item, index) => (
-                            <ProductCard imgsrc={item} prodName={loadedProductName[index]} prodPrice={loadedProductPrice[index]} key={index} onFunc={props.onFuncme} />
+                        allresponse.map((item, index) => (
+                            <ProductCard imgsrc={item.mainImage}
+                                prodName={item.name}
+                                prodPrice={item.price}
+                                key={index}
+                                origdata={item}
+                                onFunc={props.onFuncme} />
                         ))
                     }
                 </div>
